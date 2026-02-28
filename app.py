6211b375480d74eecdbbd246d59756e742bde2fe
Ashutosh Kumar Gautam <125810482+Ashu29y@users.noreply.github.com>
2026-02-28 16:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ st.set_page_config(
 
 
 # -------------------- LOAD MODEL --------------------
-model_path = os.path.join(os.getcwd(), "model.pkl")
-model = pk.load(open(model_path, "rb"))
+model = pk.load(open("model.pkl", "rb"))
 cars_data = pd.read_csv('Cardetails.csv')
 
 
@@ -222,5 +221,6 @@ Developed by Ashutosh Kumar Gautam | Machine Learning Portfolio Project | 2026
 </div>
 
 """, unsafe_allow_html=True)
+
 
 
